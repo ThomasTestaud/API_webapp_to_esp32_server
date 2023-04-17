33563f96e4ab5ec7b0f12c0e0af74319055868db
Thomas Testaud <thomas.testaud@hotmail.fr>
2023-04-17 15:29:31 +0200

    more work

--- index.php
+++ index.php
@@ -3,13 +3,19 @@
 session_start();
 
 
+//SETUP SESSION
+if (!isset($_SESSION['text']) && !isset($_POST['text'])) {
+    $_POST['text'] = "Une balise d'ouverture sera surlignée en vert &lt;dnt&gt; et surlignera en bleu le texte jusqu'à la fermeture de celle-ci. &lt;/dnt&gt; Une mauvaise balise &lt;dtn&gt; sera surlignée en rouge.";
+}
+
 //ROUTER
 if (array_key_exists('route', $_GET)) {
 
     switch ($_GET['route']) {
 
         case 'home':
-            echo ('home');
+            $template = 'home.phtml';
+            require_once 'views/layout.phtml';
             break;
 
         case 'api':
